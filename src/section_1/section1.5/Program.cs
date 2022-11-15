1df6abc16492e7a80b6014d0ba2694db84d5ac2d
slorello89 <slorello89@gmail.com>
2022-11-15 16:26:40 -0500

    adding pipelining/connections sections

---
+++ src/section_1/section1.5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using StackExchange.Redis;
+
+var options = new ConfigurationOptions
+{
+    EndPoints = new EndPointCollection { "localhost:6379" }
+};
+
+var muxer = ConnectionMultiplexer.Connect(options);
+var db = muxer.GetDatabase();
+
+var stopwatch = Stopwatch.StartNew();
+// un-pipelined commands incur the added cost of an extra round trip
+for (var i = 0; i < 1000; i++)
+{
+    await db.PingAsync();
+}
+
+Console.WriteLine($"1000 un-pipelined commands took: {stopwatch.ElapsedMilliseconds}ms to execute");
+
+// If we run out async tasks to StackExchange.Redis concurrently, the library
+// will automatically manage pipelining of these commands to Redis, making
+// them significantly more performant as we remove most of the round trips to Redis.
+var pingTasks = new List<Task<TimeSpan>>();
+
+// restart stopwatch
+stopwatch.Restart();
+
+for (var i = 0; i < 1000; i++)
+{
+    pingTasks.Add(db.PingAsync());
+}
+
+await Task.WhenAll(pingTasks);
+
+Console.WriteLine($"1000 automatically pipelined tasks took: {stopwatch.ElapsedMilliseconds}ms to execute, first result: {pingTasks[0].Result}");
+
+// clear our ping tasks list.
+pingTasks.Clear();
+
+// Batches allow you to more intentionally group together the commands that you want to send to Redis.
+// If you employee a batch, all commands in the batch will be sent to Redis in one contiguous block, with no
+// other commands from the client interleaved. Of course, if there are other clients to Redis, commands from those
+// other clients may be interleaved with your batched commands.
+var batch = db.CreateBatch();
+
+// restart stopwatch
+stopwatch.Restart();
+
+for (var i = 0; i < 1000; i++)
+{
+    pingTasks.Add(batch.PingAsync());
+}
+
+batch.Execute();
+await Task.WhenAll(pingTasks);
+Console.WriteLine($"1000 batched commands took: {stopwatch.ElapsedMilliseconds}ms to execute, first result: {pingTasks[0].Result}");
