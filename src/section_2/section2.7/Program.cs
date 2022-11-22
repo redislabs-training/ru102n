1b77d29ec5a5f01596f6517527de808201177f06
slorello89 <slorello89@gmail.com>
2022-11-22 09:21:22 -0500

    scripting

---
+++ src/section_2/section2.7/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+
+var muxer = ConnectionMultiplexer.Connect("localhost");
+var db = muxer.GetDatabase();
+
+var scriptText = @"
+    local id = redis.call('incr', @id_key)
+    local key = 'key:' .. id
+    redis.call('set', key, @value)
+    return key
+";
+
+var script = LuaScript.Prepare(scriptText);
+
+var key1 = db.ScriptEvaluate(script, new {id_key=(RedisKey)"autoIncrement", value="A String Value"});
+var key2 = db.ScriptEvaluate(script, new {id_key=(RedisKey)"autoIncrement", value="Another String Value"});
+
+Console.WriteLine($"Key 1: {key1}");
+Console.WriteLine($"Key 2: {key2}");
