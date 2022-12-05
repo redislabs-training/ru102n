a099e2c1be667939be1c90a257017729b7991f7f
slorello89 <slorello89@gmail.com>
2022-12-05 15:17:12 -0500

    graph

---
+++ src/section_4/section4.3/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NRedisGraph;
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+var muxer = ConnectionMultiplexer.Connect("localhost");
+var db = muxer.GetDatabase();
+var graph = new RedisGraph(db);
+db.KeyDelete("pets");
+
+var createBobResult = await graph.QueryAsync("pets", "CREATE(:human{name:'Bob',age:32})");
+await graph.QueryAsync("pets", "CREATE(:human{name:'Alice',age:30})");
+
+Console.WriteLine($"Nodes Created:{createBobResult.Statistics.NodesCreated}");
+Console.WriteLine($"Properties Set:{createBobResult.Statistics.PropertiesSet}");
+Console.WriteLine($"Labels Created:{createBobResult.Statistics.LabelsAdded}");
+Console.WriteLine($"Operation took:{createBobResult.Statistics.QueryInternalExecutionTime}");
+
+await graph.QueryAsync("pets", "CREATE(:pet{name:'Honey',age:5,species:'canine',breed:'Greyhound'})");
+
+await graph.QueryAsync("pets",
+    "MATCH(a:human),(p:pet) WHERE(a.name='Bob' and p.name='Honey') CREATE (a)-[:OWNS]->(p)");
+    
+await graph.QueryAsync("pets",
+    "MATCH(a:human),(p:pet) WHERE(a.name='Alice' and p.name='Honey') CREATE (a)-[:WALKS]->(p)");
+await graph.QueryAsync("pets",
+    "MATCH(a:human),(p:pet) WHERE(a.name='Bob' and p.name='Honey') CREATE (a)-[:WALKS]->(p)");
+    
+var matches = await graph.QueryAsync("pets", "MATCH(a:human),(p:pet) where (a)-[:OWNS]->(p) and p.name='Honey' return a");
+
+var record = matches.First();
+
+Console.WriteLine($"Honey's owner nodes: {record}");
+
+matches = await graph.QueryAsync("pets", "MATCH(a:human),(p:pet) where (a)-[:WALKS]->(p) and p.name='Honey' return a");
+
+foreach (var rec in matches)
+{
+    var node = (Node)rec.Values.First();
+    Console.WriteLine($"{node.PropertyMap["name"].Value} walks honey");
+}
+
+matches = await graph.QueryAsync("pets", "MATCH(a:human),(p:pet) where (a)-[:OWNS]->(p) and p.species='canine' and a.name='Bob' return p");
+
+foreach (var rec in matches)
+{
+    var dogs = rec.Values.Select(x=>(Node)x).Select(x=>x.PropertyMap["name"].Value.ToString());
+    Console.WriteLine($"Bob's dogs are: {string.Join(", ", dogs)}");
+}
+
