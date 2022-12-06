ca75d8cd218d683a757775da437ef9c008a00e63
slorello89 <slorello89@gmail.com>
2022-12-06 10:01:40 -0500

    object modeling

---
+++ src/section_5/section5.1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Redis.OM;
+using section5._1;
+
+var provider = new RedisConnectionProvider("redis://localhost:6379");
+
+provider.Connection.DropIndexAndAssociatedRecords(typeof(Sale));
+provider.Connection.DropIndexAndAssociatedRecords(typeof(Employee));
+
+await provider.Connection.CreateIndexAsync(typeof(Sale));
+await provider.Connection.CreateIndexAsync(typeof(Employee));
