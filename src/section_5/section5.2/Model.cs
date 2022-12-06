2c6562c42fa2ceb916c3c8a20d99bd86983db3ab
slorello89 <slorello89@gmail.com>
2022-12-06 13:44:18 -0500

    insertions

---
+++ src/section_5/section5.2/Model.cs
@@ -0,0 +1,44 @@
+﻿using Redis.OM.Modeling;
+
+namespace section5._2;
+
+[Document(StorageType = StorageType.Json, Prefixes = new []{"Employee"}, IndexName = "employees")]
+public class Employee
+{
+    [RedisIdField]
+    [Indexed]
+    public string? Id { get; set; }
+    [Indexed]
+    public List<string>? Sales { get; set; }
+    [Indexed(JsonPath = "$.Location")]
+    [Indexed(JsonPath = "$.PostalCode")]
+    public Address? Address { get; set; }
+    [Indexed]
+    public string? Name { get; set; }
+}
+
+[Document(StorageType = StorageType.Json)]
+public class Sale
+{
+    [RedisIdField]
+    [Indexed]
+    public string? Id { get; set; }
+    [Indexed]
+    public string? EmployeeId { get; set; }
+    [Indexed]
+    public int Total { get; set; }
+    [Indexed(CascadeDepth = 2)]
+    public Address? Address { get; set; }
+}
+
+public class Address
+{
+    [Searchable]
+    public string? StreetAddress { get; set; }
+    [Indexed]
+    public string? PostalCode { get; set; }
+    [Indexed]
+    public GeoLoc Location { get; set; }
+    [Indexed(CascadeDepth = 1)]
+    public Address? ForwardingAddress { get; set; }
+}
---
+++ src/section_5/section5.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Redis.OM;
+using Redis.OM.Modeling;
+using section5._2;
+
+var provider = new RedisConnectionProvider("redis://localhost:6379");
+
+provider.Connection.DropIndexAndAssociatedRecords(typeof(Sale));
+provider.Connection.DropIndexAndAssociatedRecords(typeof(Employee));
+
+await provider.Connection.CreateIndexAsync(typeof(Sale));
+await provider.Connection.CreateIndexAsync(typeof(Employee));
+
+var employees = provider.RedisCollection<Employee>();
+
+var employee = new Employee
+{
+    Name = "Steve",
+    Address = new Address
+    {
+        StreetAddress = "Main Street",
+        PostalCode = "34739",
+        Location = new GeoLoc(-81.006, 27.872)
+    },
+    Sales = new List<string>()
+};
+var key = employees.Insert(employee);
+
+Console.WriteLine($"Employee Id: {employee.Id}");
+Console.WriteLine($"Key Name: {key}");
+
+var sale = new Sale
+{
+    Id = Guid.NewGuid().ToString(),
+    Address = new Address
+    {
+        StreetAddress = "Pinewood Ave",
+        PostalCode = "10001",
+        Location = new GeoLoc( -73.991, 40.753)
+    },
+    EmployeeId = employee.Id,
+    Total = 5000,
+};
+
+key = provider.Connection.Set(sale, TimeSpan.FromMinutes(5));
+Console.WriteLine($"Sale Id: {sale.Id}");
+Console.WriteLine($"Key Name: {key}");
