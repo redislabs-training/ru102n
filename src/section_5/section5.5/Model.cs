115ca87a5f3cfa1f102ccb4e03618f2dda159714
slorello89 <slorello89@gmail.com>
2022-12-08 09:41:37 -0500

    aggregations

---
+++ src/section_5/section5.5/Model.cs
@@ -0,0 +1,52 @@
+﻿using Redis.OM.Modeling;
+
+namespace section5._5;
+
+[Document(StorageType = StorageType.Json, Prefixes = new []{"Employee"}, IndexName = "employees")]
+public class Employee
+{
+    [RedisIdField]
+    [Indexed]
+    public string? Id { get; set; }
+    [Indexed]
+    public List<string>? Sales { get; set; }
+    [Indexed(JsonPath = "$.Location", Aggregatable = true)]
+    [Indexed(JsonPath = "$.PostalCode")]
+    public Address? Address { get; set; }
+    [Indexed(Sortable = true)]
+    public string? Name { get; set; }
+    [Indexed(Sortable = true)] 
+    public int Age { get; set; }
+
+    [Indexed(Aggregatable = true)]
+    public long TotalSales { get; set; }
+    
+    [Indexed(Aggregatable = true)]
+    public double SalesAdjustment { get; set; }
+}
+
+[Document(StorageType = StorageType.Json)]
+public class Sale
+{
+    [RedisIdField]
+    [Indexed]
+    public string? Id { get; set; }
+    [Indexed(Aggregatable = true)]
+    public string? EmployeeId { get; set; }
+    [Indexed(Aggregatable = true)]
+    public int Total { get; set; }
+    [Indexed(CascadeDepth = 2)]
+    public Address? Address { get; set; }
+}
+
+public class Address
+{
+    [Searchable]
+    public string? StreetAddress { get; set; }
+    [Indexed]
+    public string? PostalCode { get; set; }
+    [Indexed(Sortable = true)]
+    public GeoLoc Location { get; set; }
+    [Indexed(CascadeDepth = 1)]
+    public Address? ForwardingAddress { get; set; }
+}
---
+++ src/section_5/section5.5/Program.cs
@@ -0,0 +1,105 @@
+﻿using Redis.OM;
+using Redis.OM.Aggregation.AggregationPredicates;
+using Redis.OM.Modeling;
+using section5._5;
+
+var provider = new RedisConnectionProvider("redis://localhost:6379");
+
+provider.Connection.DropIndexAndAssociatedRecords(typeof(Sale));
+provider.Connection.DropIndexAndAssociatedRecords(typeof(Employee));
+
+await provider.Connection.CreateIndexAsync(typeof(Sale));
+await provider.Connection.CreateIndexAsync(typeof(Employee));
+
+var employees = provider.RedisCollection<Employee>();
+
+// create a couple of employees
+var alice = new Employee
+{
+    Name = "Alice",
+    Age = 45,
+    Address = new Address { StreetAddress = "Elm Street", Location = new GeoLoc(-81.957, 27.058), PostalCode = "34269" },
+    Sales = new List<string>(),
+    SalesAdjustment = 1.1
+};
+
+var bob = new Employee
+{
+    Name = "Bob",
+    Age = 60,
+    Address = new Address() { StreetAddress = "Bleecker Street", Location = new GeoLoc(-74.003, 40.732), PostalCode = "10014" },
+    Sales = new List<string>(),
+    SalesAdjustment = .9
+};
+
+await employees.InsertAsync(bob);
+await employees.InsertAsync(alice);
+
+var sales = provider.RedisCollection<Sale>();
+var saleInsertTasks = new List<Task<string>>();
+var random = new Random();
+
+foreach (var emp in new[] { alice,bob })
+{
+    for (var i = 0; i < 100; i++)
+    {
+        var sale = new Sale
+        {
+            Total = random.Next(1000, 30000),
+            EmployeeId = emp.Id
+        };
+        saleInsertTasks.Add(sales.InsertAsync(sale));
+        emp.Sales!.Add(sale.Id!);
+        emp.TotalSales += sale.Total;
+
+    }
+}
+
+await Task.WhenAll(saleInsertTasks);
+await employees.UpdateAsync(alice);
+await employees.UpdateAsync(bob);
+
+var saleAggregations = provider.AggregationSet<Sale>();
+
+var sumBobSales = saleAggregations.Filter(x=>x.RecordShell!.EmployeeId == bob.Id).Sum(x=>x.RecordShell!.Total);
+Console.WriteLine($"Bob's total sales: {sumBobSales}");
+
+var employeeAggregations = provider.AggregationSet<Employee>();
+var adjustedSales = employeeAggregations.Apply(x=>Math.Ceiling(x.RecordShell.SalesAdjustment * x.RecordShell.TotalSales), "ADJUSTED_SALES");
+
+foreach(var employee in adjustedSales)
+{
+    Console.WriteLine($"Adjusted Sales: {employee["ADJUSTED_SALES"]}");
+}
+
+// String functions
+
+var birthdayMessage = employeeAggregations.Apply(x=>$"Happy Birthday {x.RecordShell.Name} you're now {x.RecordShell.Age} ","BIRTHDAY_MESSAGE");
+foreach (var employee in birthdayMessage)
+{
+    Console.WriteLine($"{employee["BIRTHDAY_MESSAGE"]}");
+}
+
+
+// Geo Distance
+var empireGeoLoc = new GeoLoc(-74.0031713,40.7484396);
+
+var distanceFromEmpireStateBuilding = employeeAggregations
+                                                                .Load(x=>new {x.RecordShell.Name})
+                                                                .Apply(x=>ApplyFunctions.GeoDistance(x.RecordShell.Address.Location, empireGeoLoc),"DISTANCE_FROM_EMPIRE_STATE_BUILDING");
+foreach (var res in distanceFromEmpireStateBuilding)
+{
+    var employee = res.Hydrate();
+    Console.WriteLine($"{employee.Name} is {res["DISTANCE_FROM_EMPIRE_STATE_BUILDING"]} meters from the Empire State Building");
+}
+
+// Top Salesperson
+var topSalesId = saleAggregations
+    .GroupBy(x=>x.RecordShell.EmployeeId)
+    .Sum(x=>x.RecordShell.Total)
+    .OrderByDescending(x=>x["Total_SUM"])
+    .Take(1)
+    .First()["EmployeeId"]
+    .ToString();
+var topSalesPerson = await employees.FindByIdAsync(topSalesId);
+Console.WriteLine($"Top seller: {topSalesPerson.Name}");
